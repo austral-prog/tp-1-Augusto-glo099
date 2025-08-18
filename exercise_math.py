2cf4691e737a926b1943b2396fc5bb62c6d6c569
Augusto-glo099 <aecondehendel@mail.austral.edu.ar>
2025-08-18 11:21:57 -0300

    Update exercise_math.py
    tp1
    conde hendel augusto 
    biomedica

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,23 @@
 def math():
     a = 57
     b = 7
+    suma = a+b
+    print(suma)
+
+    diferencia = a-b
+    print(diferencia)
+
+    producto = a*b
+    print(producto)
+
+    promedio = (a+b)/2
+    print(promedio)
+
+    cociente_entero = a//b
+    print(cociente_entero)
+
+    resto = a % b
+    print(resto)
+
+    division_real = a/b
+    print(divicion_real)
